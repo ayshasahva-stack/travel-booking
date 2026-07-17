57f6e4c8f8a29cb0c7d92aa226c51f8b8496d688
aysha sahva <ayshasahva@gmail.com>
2026-07-18 02:02:34 +0530

    navbar created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import { FaPlaneDeparture } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <div>
-      <nav>
-        Navbar
-      </nav>
-    </div>
-  )
+    <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <FaPlaneDeparture className="text-blue-600 text-2xl" />
+          <h1 className="text-2xl font-bold text-blue-600">
+            TravelEase
+          </h1>
+        </div>
+
+        {/* Navigation Links */}
+        <div className="flex gap-8">
+         <NavLink to='/' className={({isActive})=>isActive?"text-blue-600 font-semibold":"text-gray-700"}>Home</NavLink>
+         <NavLink to='/destinations' className={({isActive})=>isActive?"text-blue-600 font-semibold":"text-gray-700"}>Destinations</NavLink>
+         <NavLink to='/hotels' className={({isActive})=>isActive?"text-blue-600 font-semibold":"text-gray-700"}>Hotels</NavLink>
+         <NavLink to='/about' className={({isActive})=>isActive?"text-blue-600 font-semibold":"text-gray-700"}>About</NavLink>
+         <NavLink to='/contact' className={({isActive})=>isActive?"text-blue-600 font-semibold":"text-gray-700"}>Contact</NavLink>
+        </div>
+
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
