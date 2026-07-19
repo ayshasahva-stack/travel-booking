eeb73bc9a320c6c346934d8e5a7fda76d1dbd0b2
aysha sahva <ayshasahva@gmail.com>
2026-07-19 23:16:07 +0530

    login updating

--- src/components/Auth/AuthModal.jsx
+++ src/components/Auth/AuthModal.jsx
@@ -8,22 +8,22 @@ function AuthModal() {
             <div className="bg-white w-[900px] h-[500px] rounded-3xl shadow-2xl overflow-hidden">
 
                 <div className="flex h-full ">
-                    <div className="flex-1 bg-white ">
+                    <div className="flex-1 bg-white p-12">
                         <h1 className="text-4xl font-bold text-teal-700">travelEase</h1>
 
-                        <h2 className="text-2xl font-bold mt-14">Welcome Back</h2>
-                        <p className="text-gray-500 mt-3 text-lg">
+                        <h2 className="text-2xl font-bold mt-6">Welcome Back</h2>
+                        <p className="text-gray-500 mt-2 text-m">
                             Sign in to continue your journey
                         </p>
 
                         {/* email */}
 
-                        <div className="mt-10">
-                            <label className="block text-gray-700 font-medium m-2">Email:</label><br />
+                        <div className="mt-6">
+                            <label className="block text-gray-700 font-medium mb-2">Email:</label>
                             <input 
                             type="email"
                             placeholder="Enter your email"
-                              className="w-[400px] border border-gray rounded px-4 py-3 focus:border-teal-600 mx-8" />
+                              className="w-full border border-gray-400 rounded-xl px-4 py-3 focus:outline-none " />
 
                         </div>
 
