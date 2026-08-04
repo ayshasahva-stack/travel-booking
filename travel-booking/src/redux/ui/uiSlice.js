588e10f71308a9fadebeb4da19496aab1e022043
aysha sahva <ayshasahva@gmail.com>
2026-08-05 02:10:00 +0530

    json-server

---
+++ travel-booking/src/redux/ui/uiSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  showAuthModal: false,
+  mode: "login", // "login" | "register"
+};
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+
+  reducers: {
+    openLogin: (state) => {
+  console.trace("openLogin");
+  state.showAuthModal = true;
+  state.mode = "login";
+},
+
+openRegister: (state) => {
+  console.trace("openRegister");
+  state.showAuthModal = true;
+  state.mode = "register";
+},
+
+closeAuthModal: (state) => {
+  console.trace("closeAuthModal");
+  state.showAuthModal = false;
+},
+setLoginMode: (state) => {
+  state.mode = "login";
+},
+  },
+});
+
+export const {
+  openLogin,
+  openRegister,
+  closeAuthModal,
+  setLoginMode,
+} = uiSlice.actions;
+
+export default uiSlice.reducer;
